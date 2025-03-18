1fb2f4641080da02a795c57c59277aef335fb68f
Jeramie <jeramie@protonmail.com>
2025-03-18 04:40:51 -0700

    fix: use env variable for api url

--- site/src/lib/paste.ts
+++ site/src/lib/paste.ts
@@ -2,7 +2,9 @@ import { decryptData, encryptData, generateEncryptionKey } from "./enc";
 import { z } from "zod";
 
 const API_BASE_URL =
-  process.env.NODE_ENV === "production" ? "/api" : "http://127.0.0.1:3001/api";
+  process.env.NODE_ENV === "production"
+    ? process.env.VITE_API_URL
+    : "http://127.0.0.1:3001/v1";
 
 // Zod schemas for validation
 const PasteSchema = z.object({
